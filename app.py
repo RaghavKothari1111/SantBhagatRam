c541430f4ad34bbf32ceae53a9f8ea3b53714c82
RaghavKothari1111 <raghavkothari1111@gmail.com>
2025-06-25 10:47:08 +0530

    .

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def donate():
     return render_template('donate.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
